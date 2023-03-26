291dcb255ecaedb2aeda2f52f78f51f828653c1f
Uttarayan <uttarayan21@gmail.com>
2023-03-26 17:32:20 +0530

    [fix] Use system dependent abbreviations

--- config/fish/conf.d/abbr.fish
+++ config/fish/conf.d/abbr.fish
@@ -1,1 +1,33 @@
 abbr t '$HOME/.config/tmux/start-tmux'
+
+if uname | grep -q Darwin
+    abbr find   gfind
+    abbr sed    gsed
+end
+
+if type -q nvim
+    abbr vim    nvim
+    abbr v      nvim
+end
+if type -q bat
+    abbr cat    bat
+end
+if type -q exa
+    abbr ls     exa
+    abbr ll     exa -l
+    abbr la     exa -la
+end
+if type -q zoxide
+    abbr cd     z
+end
+if type -q git
+    abbr g      git
+    abbr gp     git push
+    abbr gpu    git pull
+end
+if type -q evcxr
+    abbr reru   evcxr
+end
+if not type -q wget
+    abbr wget   curl -O
+end
