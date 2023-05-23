ca3370e07c0da1067eeddc102d226119a5cc46dc
Uttarayan Mondal <email@uttarayan.me>
2023-05-23 12:36:37 +0530

    [fix] Uncomment brew

--- config/fish/conf.d/00brew.fish
+++ config/fish/conf.d/00brew.fish
@@ -1,11 +1,11 @@
 if [ (uname) = "Darwin" ]
     set arch (arch)
     if [ "$arch" = "i386" ]
-        # eval /usr/local/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+        eval /usr/local/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
         /usr/local/bin/brew shellenv | source
         export PATH="/usr/local/opt/llvm/bin:$PATH"
     else
-        # eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+        eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
         /opt/homebrew/bin/brew shellenv | source
         export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
     end
