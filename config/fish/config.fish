a60c1f5b6761cd71643868d5ec761ac943bca288
Uttarayan <uttarayan21@gmail.com>
2024-02-14 01:54:24 +0530

    [feat]

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -10,4 +10,7 @@ export CARGO_TARGET_DIR="$HOME/.local/share/cargo-target"
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/fs0c131y/Projects/gcloud/google-cloud-sdk/path.fish.inc' ]; . '/Users/fs0c131y/Projects/gcloud/google-cloud-sdk/path.fish.inc'; end
 
-   alias rebuds="bluetoothctl remove XX:XX:XX:XX:XX:XX; bluetoothctl scan on;bluetoothctl pair XX:XX:XX:XX:XX:XX; bluetoothctl connect XX:XX:XX:XX:XX:XX"
+alias rebuds="bluetoothctl remove XX:XX:XX:XX:XX:XX; bluetoothctl scan on;bluetoothctl pair XX:XX:XX:XX:XX:XX; bluetoothctl connect XX:XX:XX:XX:XX:XX"
+mkdir -p ~/.config/fish/completions
+carapace --list | awk '{print $1}' | xargs -I{} touch ~/.config/fish/completions/{}.fish # disable auto-loaded completions (#185)
+carapace _carapace | source
