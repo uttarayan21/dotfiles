8df56b55d32e6921675640908c815316cdafe84b
Uttarayan <uttarayan21@gmail.com>
2024-02-14 21:14:57 +0530

    [feat] Add foot configuration

--- config/nix/home.nix
+++ config/nix/home.nix
@@ -37,7 +37,9 @@
       };
       package = pkgs.nushellFull;
       configFile.text = ''
-        show_banner: false,
+        $env.config = {
+            show_banner: false,
+        }
       '';
     };
     zoxide = {
@@ -77,6 +79,43 @@
         enable = true;
         enableFishIntegration = true;
         enableNushellIntegration = true;
+    };
+    foot = {
+      enable = true;
+      server.enable = true;
+      settings = {
+        main = {
+            shell = "${pkgs.fish.outPath}/bin/fish";
+            font = "Hasklug Nerd Font Mono:size=13";
+        };
+        colors = {
+            foreground = "f8f8f2";
+            initial-window-size-pixels="1440x800";
+            background=000000;
+            alpha=0.8;
+
+            "136" = "af8700";
+
+            regular0 ="21222c";
+            regular1 ="ff5555";
+            regular2 ="50fa7b";
+            regular3 ="f1fa8c";
+            regular4 ="bd93f9";
+            regular5 ="ff79c6";
+            regular6 ="8be9fd";
+            regular7 ="f8f8f2";
+
+            bright0 ="6272a4";
+            bright1 ="ff6e6e";
+            bright2 ="69ff94";
+            bright3 ="ffffa5";
+            bright4 ="d6acff";
+            bright5 ="ff92df";
+            bright6 ="a4ffff";
+            bright7 ="ffffff";
+
+        };
+      };
     };
 
   };
