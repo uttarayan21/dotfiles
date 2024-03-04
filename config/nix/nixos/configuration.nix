a72249a1be65a7effb5ef6d3f1748b7bec67b38d
uttarayan21 <email@uttarayan.me>
2024-03-04 17:58:01 +0530

    [feat] Rename hardware-configuration.nix

--- config/nix/nixos/configuration.nix
+++ config/nix/nixos/configuration.nix
@@ -1,10 +1,10 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{ pkgs, ... }: {
+{ pkgs, device, ... }: {
   imports = [
     # Include the results of the hardware scan.
-    ./hardware-configuration.nix
+    ./ryu.nix
   ];
 
   security.polkit.enable = true;
