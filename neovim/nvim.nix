188e6d93a0b1872205e7302d7dba68e03b82dcef
Uttarayan Mondal <email@uttarayan.me>
2024-05-14 02:12:42 +0530

    feat: Lua context

--- neovim/nvim.nix
+++ neovim/nvim.nix
@@ -78,9 +78,13 @@ in rec {
 
       comment = {
         enable = true;
-        settings.pre_hook = ''
-          require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()
-        '';
+        settings.pre_hook =
+          /*
+          lua
+          */
+          ''
+            require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()
+          '';
       };
 
       markdown-preview = {
