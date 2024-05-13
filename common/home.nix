d44ee684614e49145dc5f0f227fa7040a8232964
Uttarayan Mondal <email@uttarayan.me>
2024-05-13 23:14:30 +0530

    feat: Use atuin server

--- common/home.nix
+++ common/home.nix
@@ -122,6 +122,11 @@ in {
 
   programs = {
     atuin = {
+      settings = {
+        auto_sync = true;
+        sync_frequency = "1m";
+        sync_address = "https://atuin.darksailor.dev";
+      };
       enable = true;
       enableFishIntegration = true;
       enableNushellIntegration = true;
