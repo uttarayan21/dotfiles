f1e532278433da4054c547f386101105e89995ef
uttarayan21 <email@uttarayan.me>
2024-06-21 14:19:52 +0530

    feat: Add git ssh key signing

--- common/home.nix
+++ common/home.nix
@@ -175,6 +175,13 @@ in {
         merge.conflictStyle = "diff3";
         diff.colorMoved = "default";
         push.autoSetupRemote = true;
+        user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJfKKrX8yeIHUUury0aPwMY6Ha+BJyUR7P0Gqid90ik/";
+        gpg.format = "ssh";
+        commit.gpgsign = true;
+        pull = {
+          rebase = true;
+        };
+        "gpg \"ssh\"".program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
       };
     };
     nix-index = {
