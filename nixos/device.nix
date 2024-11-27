a4882d8057964fdc8e027cd538394066f96041c1
uttarayan21 <email@uttarayan.me>
2024-11-27 20:34:52 +0530

    feat: Remove some stuff

--- nixos/device.nix
+++ nixos/device.nix
@@ -15,38 +15,28 @@ builtins.listToAttrs (builtins.map (device: {
         inherit device;
         lanzaboote = inputs.lanzaboote;
       };
-      modules =
-        [
-          nur.nixosModules.nur
-          inputs.sops-nix.nixosModules.sops
-          {nixpkgs.overlays = overlays;}
-          ./${device.name}/configuration.nix
-          home-manager.nixosModules.home-manager
-          inputs.lanzaboote.nixosModules.lanzaboote
-          inputs.musnix.nixosModules.musnix
-          {
-            nixpkgs.config.allowUnfree = true;
-            home-manager = {
-              backupFileExtension = "bak";
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              extraSpecialArgs = {
-                inherit inputs;
-                inherit device;
-              };
-              users.${device.user}.imports = [../common/home.nix];
+      modules = [
+        nur.nixosModules.nur
+        inputs.sops-nix.nixosModules.sops
+        {nixpkgs.overlays = overlays;}
+        ./${device.name}/configuration.nix
+        home-manager.nixosModules.home-manager
+        inputs.lanzaboote.nixosModules.lanzaboote
+        inputs.musnix.nixosModules.musnix
+        {
+          nixpkgs.config.allowUnfree = true;
+          home-manager = {
+            backupFileExtension = "bak";
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = {
+              inherit inputs;
+              inherit device;
             };
-          }
-        ]
-        ++ nixpkgs.lib.optionals device.live [
-          (nixpkgs + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
-          ({pkgs, ...}: {
-            systemd.services.sshd.wantedBy = pkgs.lib.mkForce ["multi-user.target"];
-            users.users.root.openssh.authorizedKeys.keys = [
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILdV/cFR8ENy4vCHnK/dL+Ud7jOJV7+iLeAe8y5nj3xF email@uttarayan.me"
-            ];
-          })
-        ];
+            users.${device.user}.imports = [../common/home.nix];
+          };
+        }
+      ];
     };
   })
   devices)
