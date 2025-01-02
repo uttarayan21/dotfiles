9c1656ce19586bd9f9d4a9bd8ad824a0f11c83e4
uttarayan21 <email@uttarayan.me>
2025-01-03 00:40:43 +0530

    feat: Add qpwgraph to ryu

--- nixos/ryu/configuration.nix
+++ nixos/ryu/configuration.nix
@@ -248,6 +248,7 @@
     systemPackages = with pkgs; [
       #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       #  wget
+      qpwgraph
       hyprland
       xorg.xhost
       foot
