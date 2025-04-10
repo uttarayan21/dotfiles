49f70c8e18fd2e60a549dc136f04cf88923d7b8c
uttarayan21 <email@uttarayan.me>
2025-04-10 21:59:12 +0530

    chore: Remove unused bambu-studio

--- home/apps/bambu-studio.nix
+++ home/apps/bambu-studio.nix
@@ -1,10 +1,10 @@
 {pkgs, ...}: {
   home.packages = with pkgs;
     lib.optionals pkgs.stdenv.isLinux [
-      (bambu-studio.overrideAttrs (oldAttrs: {
-        nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [pkgs.cudatoolkit];
-        buildInputs = oldAttrs.buildInputs ++ [pkgs.cudatoolkit];
-      }))
+      # (bambu-studio.overrideAttrs (oldAttrs: {
+      #   nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [pkgs.cudatoolkit];
+      #   buildInputs = oldAttrs.buildInputs ++ [pkgs.cudatoolkit];
+      # }))
       (orca-slicer.overrideAttrs (oldAttrs: {
         nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [pkgs.cudatoolkit];
         buildInputs = oldAttrs.buildInputs ++ [pkgs.cudatoolkit];
