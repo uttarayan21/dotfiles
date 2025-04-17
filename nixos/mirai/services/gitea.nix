15f86a0fae13fbad60b9a6c53ea7bb578173751e
uttarayan21 <email@uttarayan.me>
2025-04-17 17:18:21 +0530

    feat: Add authelia

--- nixos/mirai/services/gitea.nix
+++ nixos/mirai/services/gitea.nix
@@ -13,8 +13,26 @@
     };
     caddy = {
       virtualHosts."git.darksailor.dev".extraConfig = ''
+        forward_auth localhost:5555 {
+            uri /api/authz/forward-auth
+            copy_headers Remote-User Remote-Groups Remote-Email Remote-Name
+        }
         reverse_proxy localhost:3000
       '';
+    };
+    authelia = {
+      instances.darksailor = {
+        settings = {
+          access_control = {
+            rules = [
+              {
+                domain = "git.darksailor.dev";
+                policy = "one_factor";
+              }
+            ];
+          };
+        };
+      };
     };
   };
 }
