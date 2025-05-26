ca2702256c799e5b277382ebc85cdd3aff6962b8
uttarayan21 <email@uttarayan.me>
2025-05-26 13:41:28 +0300

    feat: Disable zerotier

--- nixos/mirai/services/zerotier.nix
+++ nixos/mirai/services/zerotier.nix
@@ -1,24 +1,24 @@
 {config, ...}: {
-  sops = {
-    secrets."zerotier/api_key".owner = config.systemd.services.zeronsd-abfd31bd4766754d.serviceConfig.User;
-  };
-  services.zerotierone = {
-    enable = true;
-    port = 9994;
-    joinNetworks = [
-      "abfd31bd4766754d"
-    ];
-  };
-  services.zeronsd = {
-    servedNetworks = {
-      abfd31bd4766754d = {
-        settings = {
-          log_level = "trace";
-          local_url = "http://127.0.0.1:9994";
-          domain = "zt.darksailor.dev";
-          token = config.sops.secrets."zerotier/api_key".path;
-        };
-      };
-    };
-  };
+  # sops = {
+  #   secrets."zerotier/api_key".owner = config.systemd.services.zeronsd-abfd31bd4766754d.serviceConfig.User;
+  # };
+  # services.zerotierone = {
+  #   enable = true;
+  #   port = 9994;
+  #   joinNetworks = [
+  #     "abfd31bd4766754d"
+  #   ];
+  # };
+  # services.zeronsd = {
+  #   servedNetworks = {
+  #     abfd31bd4766754d = {
+  #       settings = {
+  #         log_level = "trace";
+  #         local_url = "http://127.0.0.1:9994";
+  #         domain = "zt.darksailor.dev";
+  #         token = config.sops.secrets."zerotier/api_key".path;
+  #       };
+  #     };
+  #   };
+  # };
 }
