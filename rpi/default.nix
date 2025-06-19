bc5c1f93856c575737420c4b08e195711a22d9a0
uttarayan21 <email@uttarayan.me>
2025-06-19 23:53:21 +0530

    feat: added configuration

--- rpi/default.nix
+++ rpi/default.nix
@@ -16,28 +16,26 @@
           stablePkgs = inputs.nixpkgs-stable.legacyPackages.${device.system};
         };
         modules = [
+          {nixpkgs.overlays = overlays;}
           nur.modules.nixos.default
           inputs.sops-nix.nixosModules.sops
-          inputs.disko.nixosModules.disko
-          {nixpkgs.overlays = overlays;}
-          ./${device.name}/configuration.nix
           home-manager.nixosModules.home-manager
           inputs.lanzaboote.nixosModules.lanzaboote
-          inputs.musnix.nixosModules.musnix
           inputs.arion.nixosModules.arion
+          ./configuration.nix
           {
             nixpkgs.config.allowUnfree = true;
-            home-manager = {
-              backupFileExtension = "bak";
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              extraSpecialArgs = {
-                inherit inputs;
-                inherit device;
-                stablePkgs = inputs.nixpkgs-stable.legacyPackages.${device.system};
-              };
-              users.${device.user}.imports = [../home];
-            };
+            # home-manager = {
+            #   backupFileExtension = "bak";
+            #   useGlobalPkgs = true;
+            #   useUserPackages = true;
+            #   extraSpecialArgs = {
+            #     inherit inputs;
+            #     inherit device;
+            #     stablePkgs = inputs.nixpkgs-stable.legacyPackages.${device.system};
+            #   };
+            #   users.${device.user}.imports = [./home];
+            # };
           }
         ];
       }
