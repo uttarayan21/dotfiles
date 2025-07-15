d5fc84f069e2e6de4767a02a99d76459ad001a44
uttarayan21 <email@uttarayan.me>
2025-07-16 03:46:00 +0530

    feat: Added jellyseerr

--- nixos/tsuba/services/jellyfin.nix
+++ nixos/tsuba/services/jellyfin.nix
@@ -1,8 +1,18 @@
-{...}: {
+{
+  unstablePkgs,
+  config,
+  ...
+}: {
   services = {
-    jellyseerr.enable = true;
-    # jellyfin.enable = true;
+    jellyseerr = {
+      enable = true;
+      package = unstablePkgs.jellyseerr;
+    };
     caddy = {
+      virtualHosts."jellyseerr.tsuba.darksailor.dev".extraConfig = ''
+        import hetzner
+        reverse_proxy localhost:${builtins.toString config.services.jellyseerr.port}
+      '';
       virtualHosts."jellyfin.tsuba.darksailor.dev".extraConfig = ''
         import hetzner
         reverse_proxy localhost:8096
