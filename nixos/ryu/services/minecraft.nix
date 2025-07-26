827ba18f6f455a3252443efa3c1be099155da4ed
uttarayan21 <email@uttarayan.me>
2025-07-26 20:51:50 +0530

    feat: Added minecraft servers

--- nixos/ryu/services/minecraft.nix
+++ nixos/ryu/services/minecraft.nix
@@ -1,8 +1,17 @@
 {
   pkgs,
   inputs,
+  config,
   ...
 }: {
+  sops = {
+    secrets."minecraft/craftmine".owner = "minecraft";
+    templates = {
+      "craftmine.env".content = ''
+        CRAFTMINE_RCON_PASSWORD=${config.sops.placeholder."minecraft/craftmine"}
+      '';
+    };
+  };
   services = let
     whitelist = {
       "AbhinavSE" = "8b6c052e-69b3-4bee-b9dc-12eb94653c9e";
@@ -12,12 +21,14 @@
     };
   in {
     minecraft-servers = {
-      enable = false;
+      enable = true;
       eula = true;
       openFirewall = true;
+      environmentFile = config.sops.templates."craftmine.env".path;
       servers.fabric = {
         inherit whitelist;
         enable = true;
+        enableRcon = true;
         jvmOpts = "-Xmx4G -Xms4G";
         package = pkgs.fabricServers.fabric-1_21_1;
         serverProperties = {
@@ -26,6 +37,29 @@
           level-seed = "4504535438041489910";
           view-distance = 24;
           white-list = true;
+        };
+      };
+      servers.craftmine = {
+        inherit whitelist;
+        enable = true;
+        jvmOpts = "-Xmx4G -Xms4G";
+        package = let
+          getJavaVersion = v: (builtins.getAttr "openjdk${toString v}" pkgs.javaPackages.compiler).headless;
+        in
+          pkgs.minecraft-server.override {
+            url = "https://piston-data.mojang.com/v1/objects/4527a9019e37e001770787e4523b505f79cac4c5/server.jar";
+            sha1 = "sha1-RSepAZ434AF3B4fkUjtQX3nKxMU=";
+            version = "25w14craftmine";
+            jre_headless = getJavaVersion 21;
+          };
+
+        serverProperties = {
+          enable-rcon = true;
+          "rcon.password" = "@CRAFTMINE_RCON_PASSWORD@";
+          motd = "Servius's Craftmine Server";
+          server-port = 25569;
+          white-list = true;
+          view-distance = 32;
         };
       };
     };
