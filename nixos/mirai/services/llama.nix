19b977d66e824f62401c99b8851a85c1e380dbc6
uttarayan21 <email@uttarayan.me>
2025-08-08 10:28:27 +0530

    feat: Use lmstudio-community version for gpt-oss-20b

--- nixos/mirai/services/llama.nix
+++ nixos/mirai/services/llama.nix
@@ -23,8 +23,8 @@
       enable = true;
       port = 11435;
       model = pkgs.fetchurl {
-        url = "https://huggingface.co/unsloth/gpt-oss-20b-GGUF/resolve/main/gpt-oss-20b-F16.gguf";
-        sha256 = "sha256-vE1SpG4diQiP88u0viGnyZ8LtotTUU19UGecnwfjOkE=";
+        url = "https://huggingface.co/lmstudio-community/gpt-oss-20b-GGUF/resolve/main/gpt-oss-20b-MXFP4.gguf";
+        sha256 = "65d06d31a3977d553cb3af137b5c26b5f1e9297a6aaa29ae7caa98788cde53ab";
       };
       package = pkgs.ik_llama;
     };
