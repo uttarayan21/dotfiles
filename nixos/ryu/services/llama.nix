07d4e225cc40f163b2069d92166b8bd51f53f7fe
uttarayan21 <email@uttarayan.me>
2025-08-08 19:28:24 +0530

    feat: Added lmstudio-community version of gpt-oss

--- nixos/ryu/services/llama.nix
+++ nixos/ryu/services/llama.nix
@@ -10,11 +10,10 @@
       enable = true;
       port = 11435;
       model = pkgs.fetchurl {
-        url = "https://huggingface.co/unsloth/gpt-oss-20b-GGUF/resolve/main/gpt-oss-20b-F16.gguf";
-        # sha256 = "sha256-vE1SpG4diQiP88u0viGnyZ8LtotTUU19UGecnwfjOkE=";
-        sha256 = "sha256-kpeennNi4+MbFTuINkRi8CI9srQj7Q0eyKysG/4cbmI=";
+        url = "https://huggingface.co/lmstudio-community/gpt-oss-20b-GGUF/resolve/main/gpt-oss-20b-MXFP4.gguf";
+        sha256 = "65d06d31a3977d553cb3af137b5c26b5f1e9297a6aaa29ae7caa98788cde53ab";
       };
-      package = pkgs.ik_llama;
+      # package = pkgs.ik_llama;
     };
     # caddy = {
     #   virtualHosts."llama.ryu.darksailor.dev".extraConfig = ''
