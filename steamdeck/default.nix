dfe8cac525f66d1d8cbc48bfaaea9bd554b6467e
uttarayan21 <email@uttarayan.me>
2025-08-09 17:14:09 +0530

    feat: Added a echo cp command for tailscaled

--- steamdeck/default.nix
+++ steamdeck/default.nix
@@ -10,21 +10,24 @@
   programs = {
     home-manager.enable = true;
     bash.enable = true;
-    # fish.enable = true;
+    helix = {
+      enable = true;
+      package = pkgs.evil-helix;
+    };
   };
   home = {
     username = "deck";
     homeDirectory = "/home/deck";
     packages = with pkgs; [
       _1password-cli
-      (nixvim.makeNixvim (import ../neovim))
+      just
     ];
     stateVersion = "24.11";
     activation.tailscale-service = let
       tailscale_service = pkgs.writeText "tailscaled.service" (builtins.replaceStrings ["/usr/bin/tailscaled"] ["${pkgs.tailscale}/bin/tailscaled"] (builtins.readFile ./tailscaled.service));
     in
       lib.hm.dag.entryAfter ["writeBoundary"] ''
-        run cp ${tailscale_service} /etc/systemd/system/tailscaled.service
+        run echo cp ${tailscale_service} /etc/systemd/system/tailscaled.service
       '';
   };
 }
