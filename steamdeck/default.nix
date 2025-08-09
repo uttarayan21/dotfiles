7c2cbfd2b09f31c5a73331c7e4d4e1917c64e11d
uttarayan21 <email@uttarayan.me>
2025-08-09 17:52:03 +0530

    feat: Added nextcloudcmd

--- steamdeck/default.nix
+++ steamdeck/default.nix
@@ -6,15 +6,31 @@
 }: {
   imports = [
     ./tailscale.nix
+    ../home/programs/helix.nix
+    ../home/programs/sops.nix
   ];
   programs = {
     home-manager.enable = true;
-    bash.enable = true;
-    helix = {
+    bash = {
       enable = true;
-      package = pkgs.evil-helix;
+      settings = {
+      };
     };
   };
+
+  systemd.user.services = {
+    nextcloudcmd = {
+      Unit = {
+        Description = "Nextcloud Client";
+      };
+      Service = {
+        ExecStart = "${pkgs.nextcloud-client}/bin/nextcloudcmd -n /home/deck/Nextcloud https://cloud.darksailor.dev";
+        Restart = "on-failure";
+        RestartSec = "5s";
+      };
+    };
+  };
+
   home = {
     username = "deck";
     homeDirectory = "/home/deck";
@@ -22,12 +38,15 @@
       _1password-cli
       just
     ];
-    stateVersion = "24.11";
+    home.file.".ssh/rc".text = ''
+      export PATH="/nix/var/nix/profiles/default/bin:$PATH"
+    '';
     activation.tailscale-service = let
       tailscale_service = pkgs.writeText "tailscaled.service" (builtins.replaceStrings ["/usr/bin/tailscaled"] ["${pkgs.tailscale}/bin/tailscaled"] (builtins.readFile ./tailscaled.service));
     in
       lib.hm.dag.entryAfter ["writeBoundary"] ''
         run echo cp ${tailscale_service} /etc/systemd/system/tailscaled.service
       '';
+    stateVersion = "24.11";
   };
 }
