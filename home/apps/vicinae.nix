25db631be3f5eb91ca1fee957050f11bb51f7d54
uttarayan21 <email@uttarayan.me>
2025-12-01 20:14:53 +0530

    feat(vicinae): enable service based on device type

--- home/apps/vicinae.nix
+++ home/apps/vicinae.nix
@@ -1,7 +1,11 @@
-{inputs, ...}: {
+{
+  inputs,
+  device,
+  ...
+}: {
   imports = [inputs.vicinae.homeManagerModules.default];
   services.vicinae = {
-    enable = true;
+    enable = device.is "ryu";
     autoStart = true;
     extensions = [];
   };
