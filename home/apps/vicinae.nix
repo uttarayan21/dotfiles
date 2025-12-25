586250495749ab4ad1e9c97f6d0cbef061902e15
uttarayan21 <email@uttarayan.me>
2025-12-25 22:30:53 +0530

    feat: Update to latest nixpkgs

--- home/apps/vicinae.nix
+++ home/apps/vicinae.nix
@@ -7,7 +7,7 @@
   imports = [inputs.vicinae.homeManagerModules.default];
   services.vicinae = {
     enable = device.is "ryu";
-    autoStart = true;
+    systemd.autoStart = true;
     extensions = [];
     # package = pkgs.vicinae.overrideAttrs (old: {
     #   patches = [../../patches/vicinae-ctrl-np.patch];
