3beb85c474890a51a022f474f3a9f0669cadc321
servius <servius@darksailor.dev>
2026-01-21 10:41:33 +0530

    chore(flake): update flake.lock

--- nixos/ryu/apps/vr.nix
+++ nixos/ryu/apps/vr.nix
@@ -1,7 +1,8 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
-    wlx-overlay-s
-    wayvr-dashboard
+    wayvr
+    # wlx-overlay-s
+    # wayvr-dashboard
     # bs-manager
     monado-vulkan-layers
     # envision
