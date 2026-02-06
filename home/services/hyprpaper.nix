91d6863d60b73c3e78de2df52f2ebcb24a05038d
servius <servius@darksailor.dev>
2026-02-06 12:46:33 +0530

    feat: Change wallpapers

--- home/services/hyprpaper.nix
+++ home/services/hyprpaper.nix
@@ -7,8 +7,8 @@
   services.hyprpaper = let
     wallpapers = import ../../utils/wallhaven.nix {inherit pkgs;};
     nextcloudWallpapers = name: config.home.homeDirectory + "/Nextcloud/Wallpapers/" + name;
-    silksongFleas = nextcloudWallpapers "silksong-fleas.jpg";
-    silksongShadeLord = nextcloudWallpapers "silksong-shadelord.jpg";
+    # silksongFleas = nextcloudWallpapers "silksong-fleas.jpg";
+    bocchiVertical = nextcloudWallpapers "bocchi-vertical.jpg";
   in {
     enable = device.is "ryu";
     settings = {
@@ -16,7 +16,7 @@
       wallpaper = [
         {
           monitor = device.monitors.primary;
-          path = silksongShadeLord;
+          path = wallpapers.skull;
           fit_mode = "cover";
         }
         {
@@ -26,7 +26,7 @@
         }
         {
           monitor = device.monitors.tertiary;
-          path = silksongFleas;
+          path = bocchiVertical;
           fit_mode = "cover";
         }
       ];
--- home/services/wallpaperengine.nix
+++ home/services/wallpaperengine.nix
@@ -5,28 +5,28 @@
   ...
 }:
 lib.mkIf (device.is "ryu") {
-  # systemd.user.services.wallpaperengine = {
-  #   Unit = {
-  #     Description = "Linux Wallpaper Engine";
-  #     After = ["hyprland-session.target"];
-  #     Wants = ["hyprland-session.target"];
-  #     PartOf = ["hyprland-session.target"];
-  #   };
-  #
-  #   Service = {
-  #     Environment = [
-  #       "XDG_SESSION_TYPE=wayland"
-  #     ];
-  #     Type = "simple";
-  #     ExecStartPre = "${pkgs.coreutils}/bin/sleep 3";
-  #     ExecStart = "${pkgs.linux-wallpaperengine}/bin/linux-wallpaperengine --silent --no-audio-processing -f 15 --scaling fill --screen-root HDMI-A-1 --bg 2780316434";
-  #     Restart = "on-failure";
-  #     RestartSec = 5;
-  #     TimeoutStartSec = 30;
-  #   };
-  #
-  #   Install = {
-  #     WantedBy = ["hyprland-session.target"];
-  #   };
-  # };
+  systemd.user.services.wallpaperengine = {
+    Unit = {
+      Description = "Linux Wallpaper Engine";
+      After = ["hyprland-session.target"];
+      Wants = ["hyprland-session.target"];
+      PartOf = ["hyprland-session.target"];
+    };
+
+    Service = {
+      Environment = [
+        "XDG_SESSION_TYPE=wayland"
+      ];
+      Type = "simple";
+      ExecStartPre = "${pkgs.coreutils}/bin/sleep 3";
+      ExecStart = "${pkgs.linux-wallpaperengine}/bin/linux-wallpaperengine --silent --no-audio-processing -f 15 --scaling fill --screen-root HDMI-A-1 --bg 2780316434";
+      Restart = "on-failure";
+      RestartSec = 5;
+      TimeoutStartSec = 30;
+    };
+
+    Install = {
+      WantedBy = ["hyprland-session.target"];
+    };
+  };
 }
