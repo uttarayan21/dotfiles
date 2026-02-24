4e0cb41780cd641e04fd8d67a774260f4f92c697
servius <servius@darksailor.dev>
2026-02-24 17:03:46 +0530

    feat: Update to cinny 4.10.5

--- nixos/tako/services/matrix/tuwunel.nix
+++ nixos/tako/services/matrix/tuwunel.nix
@@ -18,6 +18,43 @@
     };
   };
   cinnyConfigFile = pkgs.writeText "cinny-config.json" cinnyConfig;
+  cinny = with pkgs;
+    buildNpmPackage rec {
+      pname = "cinny-unwrapped";
+      version = "4.10.5";
+
+      src = fetchFromGitHub {
+        owner = "cinnyapp";
+        repo = "cinny";
+        tag = "v${version}";
+        hash = "sha256-Napy3AcsLRDZPcBh3oq1U30FNtvoNtob0+AZtZSvcbM=";
+      };
+
+      nodejs = nodejs_22;
+
+      npmDepsHash = "sha256-2Lrd0jAwAH6HkwLHyivqwaEhcpFAIALuno+MchSIfxo=";
+
+      nativeBuildInputs = [
+        python3
+        pkg-config
+      ];
+
+      buildInputs =
+        [
+          pixman
+          cairo
+          pango
+        ]
+        ++ lib.optionals stdenv.hostPlatform.isDarwin [giflib];
+
+      installPhase = ''
+        runHook preInstall
+
+        cp -r dist $out
+
+        runHook postInstall
+      '';
+    };
 in {
   sops = {
     secrets."tuwunel/client_id" = {
@@ -80,7 +117,7 @@ in {
         root ${cinnyConfigFile}
       }
       handle {
-          root * ${pkgs.cinny}
+          root * ${cinny}
           try_files {path} /index.html
           file_server
       }
--- overlays.nix
+++ overlays.nix
@@ -9,7 +9,6 @@
     };
   };
   misc-applications = final: prev: {
-    command-runner = inputs.command-runner.packages.${prev.system}.command-runner;
     goread = final.pkgs.buildGoModule {
       pname = "goread";
       version = "v1.6.4";
