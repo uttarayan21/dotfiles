6566c7650d2c5efd749946a3218124bb43a13bd0
servius <servius@darksailor.dev>
2026-04-18 00:17:53 +0530

    feat: Added hyprpicker

--- home/apps/hyprpicker.nix
+++ home/apps/hyprpicker.nix
@@ -6,4 +6,14 @@
   home.packages = lib.optionals pkgs.stdenv.isLinux [
     pkgs.hyprpicker
   ];
+
+  xdg.desktopEntries.hyprpicker = lib.mkIf pkgs.stdenv.isLinux {
+    name = "Hyprpicker";
+    comment = "Screen color picker for Hyprland";
+    exec = "${lib.getExe pkgs.hyprpicker} --autocopy";
+    terminal = false;
+    type = "Application";
+    categories = ["Utility" "Graphics"];
+    noDisplay = false;
+  };
 }
