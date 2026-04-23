074d712a16cee89f9f5cd0cc8aab0a2a4c1257bb
servius <servius@darksailor.dev>
2026-04-24 00:23:53 +0530

    feat: Disable mangohud for bloodborne

--- home/apps/shadps4.nix
+++ home/apps/shadps4.nix
@@ -16,7 +16,7 @@ in {
   xdg.desktopEntries.bloodborne = lib.mkIf pkgs.stdenv.isLinux {
     name = "Bloodborne";
     comment = "Bloodborne via BB_Launcher with gamescope";
-    exec = "env QT_QPA_PLATFORM=xcb MANGOHUD=1 taskset -c 0-15 gamemoderun gamescope -W 2560 -H 1440 -r 120 -f --adaptive-sync --hdr-enabled --force-grab-cursor -- ${pkgs.bblauncher}/bin/BB_Launcher -n";
+    exec = "env QT_QPA_PLATFORM=xcb taskset -c 0-15 gamemoderun gamescope -W 2560 -H 1440 -r 120 -f --adaptive-sync --hdr-enabled --force-grab-cursor -- ${pkgs.bblauncher}/bin/BB_Launcher -n";
     icon = "${bloodborne-icon}";
     terminal = false;
     type = "Application";
