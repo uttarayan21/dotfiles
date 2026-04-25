85828a591a0a9d26bbf64ac1e420687b4fdc213a
servius <servius@darksailor.dev>
2026-04-25 18:01:22 +0530

    feat: Change bloodborne from bblauncher to shadps4

--- home/apps/shadps4.nix
+++ home/apps/shadps4.nix
@@ -15,8 +15,8 @@ in {
 
   xdg.desktopEntries.bloodborne = lib.mkIf pkgs.stdenv.isLinux {
     name = "Bloodborne";
-    comment = "Bloodborne via BB_Launcher with gamescope";
-    exec = "env QT_QPA_PLATFORM=xcb taskset -c 0-15 gamemoderun gamescope -W 2560 -H 1440 -r 120 -f --adaptive-sync --hdr-enabled --force-grab-cursor -- ${pkgs.bblauncher}/bin/BB_Launcher -n";
+    comment = "Bloodborne via shadPS4 with gamescope";
+    exec = "taskset -c 0-15 gamemoderun gamescope -W 2560 -H 1440 -r 90 -f --adaptive-sync --hdr-enabled --force-grab-cursor -- ${pkgs.shadps4-qt}/bin/shadps4 -g /home/servius/Games/PS4/Bloodborne/CUSA00900/eboot.bin";
     icon = "${bloodborne-icon}";
     terminal = false;
     type = "Application";
