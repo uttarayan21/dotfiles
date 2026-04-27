21c6e3a43a652e79ff555acc1ad0657ca02642dc
servius <servius@darksailor.dev>
2026-04-28 01:41:51 +0530

    fix: Update caddy hash

--- overlays/networking.nix
+++ overlays/networking.nix
@@ -1,6 +1,6 @@
 {inputs, ...}: final: prev: {
   caddyWithCloudflare = inputs.nixpkgs.legacyPackages.${prev.stdenv.hostPlatform.system}.caddy.withPlugins {
     plugins = ["github.com/caddy-dns/cloudflare@v0.2.2"];
-    hash = "sha256-7DGnojZvcQBZ6LEjT0e5O9gZgsvEeHlQP9aKaJIs/Zg=";
+    hash = "sha256-VBmICI1wklu02jmgDRmmlfNc9ftK7a74uF280xzx8uc=";
   };
 }
