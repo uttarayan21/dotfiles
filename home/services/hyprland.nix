8be05d0ab751be58b9168e17dc2831af10e54df4
servius <servius@darksailor.dev>
2026-04-28 21:05:03 +0530

    fix: Remove hyprland pseudotile

--- home/services/hyprland.nix
+++ home/services/hyprland.nix
@@ -143,8 +143,7 @@
 
       dwindle = {
         # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
-        pseudotile =
-          true; # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+        # pseudotile = true; # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
         preserve_split = true; # you probably want this
       };
 
