28db965bcec40ea1b69bce47fff536b205643106
servius <servius@darksailor.dev>
2026-05-09 23:30:03 +0530

    feat: Use xcb for azahar

--- nixos/ryu/games/azahar.nix
+++ nixos/ryu/games/azahar.nix
@@ -1,3 +1,13 @@
-{pkgs, ...}: {
-  environment.systemPackages = [pkgs.azahar];
+{pkgs, ...}: let
+  azahar-xcb = pkgs.symlinkJoin {
+    name = "azahar";
+    paths = [pkgs.azahar];
+    nativeBuildInputs = [pkgs.makeWrapper];
+    postBuild = ''
+      wrapProgram $out/bin/azahar \
+        --set QT_QPA_PLATFORM xcb
+    '';
+  };
+in {
+  environment.systemPackages = [azahar-xcb];
 }
